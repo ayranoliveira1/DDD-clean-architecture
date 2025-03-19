e12b75428f4ff5bd5ba69119da58d4686b376444
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-19 11:26:15 -0300

    feat: add test unit in answer question use case

---
+++ src/domains/use-case/answer-question.spec.ts
@@ -0,0 +1,21 @@
+import { expect, test } from "vitest";
+import { AnsWerQuestionUseCase } from "./answer-question";
+import { Answer } from "../entities/answer";
+
+const fakerAnswerRepository = {
+   create: async (answer: Answer) => {
+      return;
+   },
+};
+
+test("create an answer to a question", async () => {
+   const answerTest = new AnsWerQuestionUseCase(fakerAnswerRepository);
+
+   const answer = await answerTest.excute({
+      questionId: "1",
+      instructorId: "1",
+      content: "new answer",
+   });
+
+   expect(answer.content).toEqual("new answer");
+});
