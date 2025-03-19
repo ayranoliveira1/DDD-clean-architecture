e3cd36d38cd6cf5600f8990fa0482869b7c427ba
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-19 11:25:54 -0300

    feat: add answer question use case

---
+++ src/domains/use-case/answer-question.ts
@@ -0,0 +1,28 @@
+import { Answer } from "../entities/answer";
+import { AnswerRepository } from "../repositories/answer-repository";
+
+interface AnsWerQuestionUseCaseRequest {
+   instructorId: string;
+   questionId: string;
+   content: string;
+}
+
+export class AnsWerQuestionUseCase {
+   constructor(private answerReposoty: AnswerRepository) {}
+
+   async excute({
+      instructorId,
+      questionId,
+      content,
+   }: AnsWerQuestionUseCaseRequest) {
+      const answer = new Answer({
+         content,
+         authorId: instructorId,
+         questionId,
+      });
+
+      await this.answerReposoty.create(answer);
+
+      return answer;
+   }
+}
