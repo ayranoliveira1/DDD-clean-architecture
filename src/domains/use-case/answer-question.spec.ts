6a8c1c681a16c30feb483e70dd43169c693a8061
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-20 11:40:18 -0300

    chore: ensure tests work with the new import format

--- src/domains/use-case/answer-question.spec.ts
+++ src/domains/use-case/answer-question.spec.ts
@@ -1,21 +1,20 @@
-import { expect, test } from "vitest";
 import { AnsWerQuestionUseCase } from "./answer-question";
 import { Answer } from "../entities/answer";
 
 const fakerAnswerRepository = {
-   create: async (answer: Answer) => {
-      return;
-   },
+  create: async (answer: Answer) => {
+    return;
+  },
 };
 
 test("create an answer to a question", async () => {
-   const answerTest = new AnsWerQuestionUseCase(fakerAnswerRepository);
+  const answerTest = new AnsWerQuestionUseCase(fakerAnswerRepository);
 
-   const answer = await answerTest.excute({
-      questionId: "1",
-      instructorId: "1",
-      content: "new answer",
-   });
+  const answer = await answerTest.excute({
+    questionId: "1",
+    instructorId: "1",
+    content: "new answer",
+  });
 
-   expect(answer.content).toEqual("new answer");
+  expect(answer.content).toEqual("new answer");
 });
