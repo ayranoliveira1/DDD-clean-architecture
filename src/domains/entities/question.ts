05ca1a862cc9e1ef90b219f4a360a88ef1b70e2d
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-20 11:23:59 -0300

    feat: add getters and setters in question entity

--- src/domains/entities/question.ts
+++ src/domains/entities/question.ts
@@ -2,6 +2,7 @@ import { Slug } from "./value-objects/slug";
 import { Entity } from "../../core/entities/entity";
 import { UniqueEntityId } from "../../core/entities/unique-entity-id";
 import { Optional } from "../../core/@types/options";
+import dayjs from "dayjs";
 
 interface QuestionsProps {
   authorId: UniqueEntityId;
@@ -14,11 +15,74 @@ interface QuestionsProps {
 }
 
 export class Question extends Entity<QuestionsProps> {
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId;
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  get slug() {
+    return this.props.slug;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, "days") <= 3;
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 100).trimEnd().concat("...");
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+
+  set title(title: string) {
+    this.props.title = title;
+    this.props.slug = Slug.create(title);
+    this.touch();
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityId | undefined) {
+    this.props.bestAnswerId = bestAnswerId;
+    this.touch();
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
   static create(
-    props: Optional<QuestionsProps, "createdAt">,
+    props: Optional<QuestionsProps, "createdAt" | "slug">,
     id?: UniqueEntityId
   ) {
-    const question = new Question({ ...props, createdAt: new Date() }, id);
+    const question = new Question(
+      {
+        ...props,
+        slug: props.slug ?? Slug.create(props.title),
+        createdAt: new Date(),
+      },
+      id
+    );
 
     return question;
   }
