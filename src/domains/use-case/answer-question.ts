52490eb0e4eeb304517f40c91c333f0f4433a164
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-20 10:24:48 -0300

    feat: add unique entity id

--- src/domains/use-case/answer-question.ts
+++ src/domains/use-case/answer-question.ts
@@ -2,27 +2,27 @@ import { Answer } from "../entities/answer";
 import { AnswerRepository } from "../repositories/answer-repository";
 
 interface AnsWerQuestionUseCaseRequest {
-   instructorId: string;
-   questionId: string;
-   content: string;
+  instructorId: string;
+  questionId: string;
+  content: string;
 }
 
 export class AnsWerQuestionUseCase {
-   constructor(private answerReposoty: AnswerRepository) {}
+  constructor(private answerReposoty: AnswerRepository) {}
 
-   async excute({
-      instructorId,
-      questionId,
+  async excute({
+    instructorId,
+    questionId,
+    content,
+  }: AnsWerQuestionUseCaseRequest) {
+    const answer = new Answer({
       content,
-   }: AnsWerQuestionUseCaseRequest) {
-      const answer = new Answer({
-         content,
-         authorId: instructorId,
-         questionId,
-      });
+      authorId: instructorId,
+      questionId,
+    });
 
-      await this.answerReposoty.create(answer);
+    await this.answerReposoty.create(answer);
 
-      return answer;
-   }
+    return answer;
+  }
 }
