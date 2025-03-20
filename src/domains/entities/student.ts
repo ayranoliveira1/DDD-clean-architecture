b469385a9ac565a0405dd9c8b4b7d29c95280f1f
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-20 11:24:33 -0300

    feat: add getters and setters in student entity

--- src/domains/entities/student.ts
+++ src/domains/entities/student.ts
@@ -5,6 +5,10 @@ interface StudentProsp {
   name: string;
 }
 export class Student extends Entity<StudentProsp> {
+  get name() {
+    return this.props.name;
+  }
+
   static create(props: StudentProsp, id?: UniqueEntityId) {
     const student = new Student(props, id);
 
