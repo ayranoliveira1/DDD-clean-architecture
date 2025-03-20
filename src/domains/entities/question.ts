b7c1a02702d5f7e0278992f6f3cce5cc898a0add
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-20 10:52:49 -0300

    feat: add create method in question entity

--- src/domains/entities/question.ts
+++ src/domains/entities/question.ts
@@ -1,11 +1,25 @@
 import { Slug } from "./value-objects/slug";
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityId } from "../../core/entities/unique-entity-id";
+import { Optional } from "../../core/@types/options";
 
 interface QuestionsProps {
+  authorId: UniqueEntityId;
+  bestAnswerId?: UniqueEntityId;
   title: string;
-  slug: Slug;
   content: string;
-  authorId: string;
+  slug: Slug;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
-export class Question extends Entity<QuestionsProps> {}
+export class Question extends Entity<QuestionsProps> {
+  static create(
+    props: Optional<QuestionsProps, "createdAt">,
+    id?: UniqueEntityId
+  ) {
+    const question = new Question({ ...props, createdAt: new Date() }, id);
+
+    return question;
+  }
+}
