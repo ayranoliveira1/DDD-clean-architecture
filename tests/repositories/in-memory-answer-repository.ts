66b9439d4deef364a964b371a993bd075a256e1a
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-21 19:14:27 -0300

    feat: add method delete in repositories

--- tests/repositories/in-memory-answer-repository.ts
+++ tests/repositories/in-memory-answer-repository.ts
@@ -4,7 +4,23 @@ import { Answer } from '@/domains/forum/enterprise/entities/answer'
 export class InMemoryAnswerRepository implements AnswerRepository {
   public items: Answer[] = []
 
+  async findById(id: string) {
+    const answer = this.items.find((answer) => answer.id.toString() === id)
+
+    if (!answer) {
+      return null
+    }
+
+    return answer
+  }
+
   async create(answer: Answer) {
     this.items.push(answer)
+  }
+
+  async delete(answer: Answer) {
+    const index = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items.splice(index, 1)
   }
 }
