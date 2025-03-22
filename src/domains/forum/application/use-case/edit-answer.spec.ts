673843e4df08ce3cc58d046d9417534d640e36a2
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-22 14:49:53 -0300

    feat: add edit answer use case test unit

---
+++ src/domains/forum/application/use-case/edit-answer.spec.ts
@@ -0,0 +1,43 @@
+import { InMemoryAnswerRepository } from 'tests/repositories/in-memory-answer-repository'
+import { makeAnswer } from 'tests/factories/make-answer'
+import { EditAnswerUseCase } from './edit-answer'
+
+let inMemoryAnswerRepository = new InMemoryAnswerRepository()
+let sut = new EditAnswerUseCase(inMemoryAnswerRepository)
+
+describe('Edit Answer By Id', () => {
+  beforeAll(() => {
+    inMemoryAnswerRepository = new InMemoryAnswerRepository()
+    sut = new EditAnswerUseCase(inMemoryAnswerRepository)
+  })
+
+  it('should be able to edit a answer ', async () => {
+    const newAnswer = makeAnswer()
+
+    await inMemoryAnswerRepository.create(newAnswer)
+
+    await sut.execute({
+      answerId: newAnswer.id.toString(),
+      authorId: newAnswer.authorId.toString(),
+      content: 'new content',
+    })
+
+    expect(inMemoryAnswerRepository.items[0]).toMatchObject({
+      content: 'new content',
+    })
+  })
+
+  it('should not be able to edit a answer from another user ', async () => {
+    const newAnswer = makeAnswer()
+
+    await inMemoryAnswerRepository.create(newAnswer)
+
+    await expect(async () => {
+      await sut.execute({
+        answerId: newAnswer.id.toString(),
+        authorId: 'another-user-id',
+        content: 'new content',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
