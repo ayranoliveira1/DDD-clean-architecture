959650c0ccb27a51c888776633624ffb2ca89b87
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-22 11:59:02 -0300

    feat: add edit question use case

---
+++ src/domains/forum/application/use-case/edit-question.ts
@@ -0,0 +1,34 @@
+import { QuestionsRepository } from '../repositories/questions-repository'
+
+interface EditQuestionUseCaseRequest {
+  questionId: string
+  authorId: string
+  title: string
+  content: string
+}
+
+export class EditQuestionUseCase {
+  constructor(private questionRepository: QuestionsRepository) {}
+
+  async execute({
+    questionId,
+    authorId,
+    title,
+    content,
+  }: EditQuestionUseCaseRequest) {
+    const question = await this.questionRepository.findById(questionId)
+
+    if (!question) {
+      throw new Error('Question not found')
+    }
+
+    if (question.authorId.toString() !== authorId) {
+      throw new Error('You are not the author of this question')
+    }
+
+    question.title = title
+    question.content = content
+
+    await this.questionRepository.save(question)
+  }
+}
