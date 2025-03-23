cc5f51871882142931cb3526e24988e994e97fd5
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-23 18:59:28 -0300

    feat: add delete answer comment use case test unit

---
+++ src/domains/forum/application/use-case/delete-answer-comment.spec.ts
@@ -0,0 +1,39 @@
+import { makeAnswerComment } from 'tests/factories/make-answer-comment'
+import { DeleteAnswerCommentUseCase } from './delete-answer-comment'
+import { InMemoryAnswerCommentsRepository } from 'tests/repositories/in-memory-answer-comments-repository'
+
+let inMemoryAnswerCommentRepository = new InMemoryAnswerCommentsRepository()
+let sut = new DeleteAnswerCommentUseCase(inMemoryAnswerCommentRepository)
+
+describe('Delete question comment', () => {
+  beforeEach(() => {
+    inMemoryAnswerCommentRepository = new InMemoryAnswerCommentsRepository()
+    sut = new DeleteAnswerCommentUseCase(inMemoryAnswerCommentRepository)
+  })
+
+  it('should be able to delete a question  comment ', async () => {
+    const newAnswerComment = makeAnswerComment()
+
+    await inMemoryAnswerCommentRepository.create(newAnswerComment)
+
+    await sut.execute({
+      answerCommentId: newAnswerComment.id.toString(),
+      authorId: newAnswerComment.authorId.toString(),
+    })
+
+    expect(inMemoryAnswerCommentRepository.items.length).toBe(0)
+  })
+
+  it('should not be able to delete a question comment from another user ', async () => {
+    const newAnswerComment = makeAnswerComment()
+
+    await inMemoryAnswerCommentRepository.create(newAnswerComment)
+
+    await expect(async () => {
+      await sut.execute({
+        answerCommentId: newAnswerComment.id.toString(),
+        authorId: 'another-user-id',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
