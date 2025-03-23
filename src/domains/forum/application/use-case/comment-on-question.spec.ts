a45564494e2b22a6debe2cc2c3a80822b5f3121f
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-23 12:39:06 -0300

    feat: add comment on question use case test unit

---
+++ src/domains/forum/application/use-case/comment-on-question.spec.ts
@@ -0,0 +1,52 @@
+import { makeQuestions } from 'tests/factories/make-questions'
+import { InMemoryQuestionCommentRepository } from 'tests/repositories/in-memory-question-comments'
+import { CommentOnQuestionUseCase } from './comment-on-question'
+import { InMemoryQuestionsRepository } from 'tests/repositories/in-memory-questions-repository'
+
+let inMemoryQuestionCommentRepository = new InMemoryQuestionCommentRepository()
+let inMemoryQuestionRepository = new InMemoryQuestionsRepository()
+let sut = new CommentOnQuestionUseCase(
+  inMemoryQuestionRepository,
+  inMemoryQuestionCommentRepository,
+)
+
+describe('Comment on question', () => {
+  beforeEach(() => {
+    inMemoryQuestionRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionCommentRepository = new InMemoryQuestionCommentRepository()
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionRepository,
+      inMemoryQuestionCommentRepository,
+    )
+  })
+
+  it('should be able to comment on question ', async () => {
+    const question = makeQuestions()
+
+    await inMemoryQuestionRepository.create(question)
+
+    await sut.execute({
+      questionId: question.id.toString(),
+      authorId: question.authorId.toString(),
+      content: 'Comentario teste',
+    })
+
+    expect(inMemoryQuestionCommentRepository.items[0].content).toEqual(
+      'Comentario teste',
+    )
+  })
+
+  it('should not be able to comment on teste ', async () => {
+    const question = makeQuestions()
+
+    await inMemoryQuestionRepository.create(question)
+
+    await expect(async () => {
+      await sut.execute({
+        questionId: 'invalid-id',
+        authorId: question.authorId.toString(),
+        content: 'Comentario teste',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
