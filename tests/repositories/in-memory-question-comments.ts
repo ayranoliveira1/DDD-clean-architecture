a12a1c1a2d7fbffd8a57dad7a1d46ec55810c185
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-23 13:16:23 -0300

    feat: add method delete and find by id in question comments reposiotories

--- tests/repositories/in-memory-question-comments.ts
+++ tests/repositories/in-memory-question-comments.ts
@@ -4,9 +4,25 @@ import { QuestionComment } from '@/domains/forum/enterprise/entities/question-co
 export class InMemoryQuestionCommentRepository
   implements QuestionCommentsRepositoty
 {
+  async findById(id: string) {
+    const questionComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!questionComment) {
+      return null
+    }
+
+    return questionComment
+  }
+
   public items: QuestionComment[] = []
 
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment)
+  }
+
+  async delete(questionComment: QuestionComment) {
+    const index = this.items.findIndex((item) => item.id === questionComment.id)
+
+    this.items.splice(index, 1)
   }
 }
